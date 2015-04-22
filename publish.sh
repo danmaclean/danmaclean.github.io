cc66d52cd719bb5be22b8affe16337cc0376e4ea
danmaclean <maclean.daniel@gmail.com>
2015-04-22 15:42:47 +0100

    Blog update at 2015-04-22 14:42:46

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-buster generate --domain=http://127.0.0.1:2368 --dir=/Users/macleand/src/danmaclean.github.io
+python /Users/macleand/src/buster/buster.py generate --domain=http://localhost:2368 --dir=/Users/macleand/src/danmaclean.github.io --target_domain=http://danmaclean.github.io
 buster deploy
 
